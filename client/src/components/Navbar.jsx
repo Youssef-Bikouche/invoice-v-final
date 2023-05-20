ce2a6941d0dd6ce978a5f91ec00b187273b6a24e
ElmahdiAmarjane <99350940+ElmahdiAmarjane@users.noreply.github.com>
2023-05-20 18:34:59 +0100

    Update Navbar.jsx
    add navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import '../styles/Navbar.css'
+const Navbar = () => {
+  const [headerColor, setHeaderColor] = useState('#6060eb3f');
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+     // window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const location = useLocation();
+  const currentURL = location.pathname;
+   const handleScroll = () => {
+        
+    if (window.scrollY > 200 || currentURL=='/InvoiceManu' ) { // Changer la couleur à partir de la position de scroll 100px
+      setHeaderColor('#6060ebd8'); // Changer la couleur du header
+    } else {
+      setHeaderColor('#6060eb3f'); // Réinitialiser la couleur du header
+    }
+  };
+    return ( 
+      <div className="all" style={{ backgroundColor: headerColor }}>
+            <div className="logo"> 
+            <img id="logoImg" src="/images/logoprinc.png"  /> 
+            </div>
+            <div className="nav"> 
+                <ul>
+                    <li className=''><Link to="/Home" className='same '>Home</Link> </li>
+                    <li className=''><Link to="" className='same '>Services</Link> </li>
+                    <li className=''><Link to="" className='same '>Contact</Link> </li>
+                    <li className=''><Link to="" className='same '>About us</Link> </li>
+
+                </ul>
+                </div>
+                <div className="sign">
+                <li className=''><Link to="/Login" className='login '>Login</Link> </li>
+                  <button className='btnSignUp '>Sign up</button>
+                </div>
+          
+                
+        
+        
+           
+      </div>
+     );
+  }
+   
+  export default Navbar;
